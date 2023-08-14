4a1b8e82ae7a47addaedaff331605c2e9018647d
David Thompson <dtho@carpentersgroup.co.uk>
2023-08-14 12:42:15 +0100

    Ground work to decouple function inputs from azure entites

---
+++ Shortener.Core/ShortUrlDto.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Shortener.Azure.Entities
+{
+    public class ShortUrlDto
+    {
+        [Required(AllowEmptyStrings = false)]
+        public string Url { get; set; } = null!;
+
+        public string? Title { get; set; }
+
+        [Required(AllowEmptyStrings = false)]
+        public string? ShortUrl { get; set; }
+
+        public bool? IsArchived { get; set; }
+
+        public Schedule[]? Schedules { get; set; }
+
+        public int Version { get; set; } = 0;
+
+        public int Algorithm { get; set; }
+    }
+}
---
+++ Shortener.Core/ShortUrlMapper.cs
@@ -0,0 +1,30 @@
+﻿namespace Shortener.Azure.Entities
+{
+    public static class ShortUrlMapper
+    {         public static ShortUrlDto ToDto(this ShortUrlEntity entity)
+        {
+            return new ShortUrlDto
+            {
+                Url = entity.Url,
+                Title = entity.Title,
+                ShortUrl = entity.ShortUrl,
+                IsArchived = entity.IsArchived,
+                Schedules = entity.Schedules,
+                Version = entity.Version,
+                Algorithm = entity.Algorithm
+            };
+        }
+
+        public static ShortUrlEntity ToEntity(this ShortUrlDto dto, string partitionKey)
+        {
+            ArgumentNullException.ThrowIfNull(dto.ShortUrl);
+
+            return new ShortUrlEntity(partitionKey, dto.Url, dto.ShortUrl, dto.Title ?? "", dto.Schedules)
+            {
+                IsArchived = dto.IsArchived,
+                Version = dto.Version,
+                Algorithm = dto.Algorithm
+            };
+        }
+    }
+}
