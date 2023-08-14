80389da8a42edf973dc0194df8acf7fbfdfbc70f
David Thompson <dtho@carpentersgroup.co.uk>
2023-08-14 12:25:54 +0100

    Add configuration object to bind at startup

---
+++ Shortener.Core/Shorten/Algorithms/CodeProviders/Alphabet.cs
@@ -0,0 +1,10 @@
+﻿namespace Shortener.Core.Shorten.Algorithms.CodeProviders
+{
+    public enum Alphabet
+    {
+        None,
+        NoProfanity,
+        Base64,
+        Custom
+    }
+}
---
+++ Shortener.Core/Shorten/Algorithms/ShortenerAlgorithm.cs
@@ -0,0 +1,11 @@
+﻿namespace Shortener.Core.Shorten.Algorithms
+{
+    public enum ShortenerAlgorithm
+    {
+        None,
+        IdPlusRandomFixedLength,
+        RandomFixedLength,
+        RandomExtendableLength,
+        EncodeDecode,
+    }
+}
