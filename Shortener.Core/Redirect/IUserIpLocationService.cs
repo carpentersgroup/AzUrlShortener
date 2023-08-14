38e80c0a6c5070f10e69606181bbe010a5a4b3f1
David Thompson <dtho@carpentersgroup.co.uk>
2023-08-14 12:39:46 +0100

    Move user ip location to a shared service

---
+++ Shortener.Core/Redirect/IUserIpLocationService.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.Azure.Functions.Worker.Http;
+using System.Net;
+
+namespace Shortener.Core.Redirect
+{
+    public interface IUserIpLocationService
+    {
+        IPAddress GetClientIpn(HttpRequest request);
+        IPAddress? GetClientIpn(HttpRequestData request);
+        Task<UserIpResponse> GetUserIpAsync(string ip, CancellationToken cancellationToken);
+    }
+}
