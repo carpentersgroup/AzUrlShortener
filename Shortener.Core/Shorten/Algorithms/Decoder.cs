ca800a1dfdf02ffc57dacc68fc1f4768f7660e08
David Thompson <dtho@carpentersgroup.co.uk>
2023-08-14 12:29:33 +0100

    Provide a selection of algorithms for generating short urls

---
+++ Shortener.Core/Shorten/Algorithms/Decoder.cs
@@ -0,0 +1,27 @@
+﻿using Shortener.Core.Shorten.Algorithms.CodeProviders;
+
+namespace Shortener.Core.Shorten.Algorithms
+{
+    public class Decoder : IDecode
+    {
+        private readonly AlphabetProviderBase _codeProvider;
+
+        public Decoder(AlphabetProviderBase codeProvider)
+        {
+            _codeProvider = codeProvider;
+        }
+
+        public int Decode(string code)
+        {
+            int id = 0;
+            for (int i = 0; i < code.Length; i++)
+            {
+                int index = _codeProvider.CONVERSION_CODE.IndexOf(code[i]);
+                id = id * _codeProvider.Base + index;
+            }
+            return id;
+        }
+
+        
+    }
+}
---
+++ Shortener.Core/Shorten/Algorithms/HashGenerator.cs
@@ -0,0 +1,30 @@
+﻿using Shortener.Core.Shorten.Algorithms.CodeProviders;
+
+namespace Shortener.Core.Shorten.Algorithms
+{
+    public class HashGenerator : IHash
+    {
+        private readonly AlphabetProviderBase _codeProvider;
+
+        public HashGenerator(AlphabetProviderBase codeProvider)
+        {
+            _codeProvider = codeProvider;
+        }        
+
+        public string Generate(int length)
+        {
+            var bytes = new byte[length];
+            System.Security.Cryptography.RandomNumberGenerator.Fill(bytes);
+            Span<char> characters = stackalloc char[length];
+            //iterate backwards through bytes
+            for (int i = bytes.Length - 1; i >= 0; i--)
+            {
+                //modulus to get remainder of value divided by base
+                int index = bytes[i] % _codeProvider.Base;
+                //set byte to the value of the index in the conversion code
+                characters[i] = _codeProvider.CONVERSION_CODE[index];
+            }
+            return characters.ToString();
+        }
+    }
+}
