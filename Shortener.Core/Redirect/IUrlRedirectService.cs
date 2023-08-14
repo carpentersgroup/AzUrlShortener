abd350e26b6343bdf63b7b7b87af9b547f5f2634
David Thompson <dtho@carpentersgroup.co.uk>
2023-08-14 12:41:23 +0100

    Move url redirection into its own service

---
+++ Shortener.Core/Redirect/IUrlRedirectService.cs
@@ -0,0 +1,9 @@
+﻿using System.Net;
+
+namespace Shortener.Core.Redirect
+{
+    public interface IUrlRedirectService
+    {
+        Task<Result<string, RedirectStatus>> ProcessAsync(IPAddress? ipAddress, string host, string shortUrl);
+    }
+}
